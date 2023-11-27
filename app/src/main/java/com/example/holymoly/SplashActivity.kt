2860fa5d8b1ac064784d07338e65988cc2d2ba00
nayoung16 <nykim1016@sookmyung.ac.kr>
2023-11-27 20:42:55 +0900

    Feat : Firebase google log-in

--- app/src/main/java/com/example/holymoly/SplashActivity.kt
+++ app/src/main/java/com/example/holymoly/SplashActivity.kt
@@ -1,22 +1,41 @@
 package com.example.holymoly
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val calendarImg:ImageView = findViewById(R.id.calendar_image)
+        calendarImg.setColorFilter(Color.parseColor("#FFFBFB"))
+
         // 타이머가 끝나면 내부 실행
         Handler().postDelayed(Runnable {
-            // 앱의 MainActivity로 넘어가기
-            val i = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(i)
+            val user = Firebase.auth.currentUser
+            if (user != null) {
+                // 사용자가 로그인되어 있으면 MainActivity로 이동
+
+
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // 사용자가 로그인되어 있지 않으면 AuthActivity로 이동
+                val intent = Intent(this@SplashActivity, AuthActivity::class.java)
+                startActivity(intent)
+            }
             // 현재 액티비티 닫기
             finish()
         }, 3000) // 3초
+
+
     }
 }
