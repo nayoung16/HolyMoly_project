2c9ffe245f9e9e8f346bf7719e619727f195edfc
ally432 <ally432@naver.com>
2023-11-30 15:29:00 +0900

    add activity 4th

--- app/src/main/java/com/example/holymoly/FirestoreHelper.kt
+++ app/src/main/java/com/example/holymoly/FirestoreHelper.kt
@@ -7,19 +7,7 @@ import com.google.firebase.ktx.Firebase
 class FirestoreHelper {
     private val db = Firebase.firestore
     fun addUserToFirestore(email: String, nickName: String) {
-        val user = hashMapOf(
-            "email" to email,
-            "nickName" to nickName
-        )
 
-        db.collection("hello")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
     }
 
     companion object {
