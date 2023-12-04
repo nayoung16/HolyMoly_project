2bf07d0ed1f1dd738680e2f3156331f29ea7b48d
ally432 <ally432@naver.com>
2023-12-04 19:32:42 +0900

    10

--- app/src/main/java/com/example/holymoly/ui/tab/BucketListFragment.kt
+++ app/src/main/java/com/example/holymoly/ui/tab/BucketListFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.example.holymoly.R
-import com.google.android.material.tabs.TabLayout
 
 
 class BucketListFragment : Fragment() {
@@ -25,12 +23,6 @@ class BucketListFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_bucket_list, container, false)
         // Inflate the layout for this fragment
 
-        val viewPager: ViewPager = view.findViewById(R.id.bucket_viewPager)
-        val tabLayout: TabLayout = view.findViewById(R.id.bucket_tabLayout)
-
-        val innerPagerAdapter = BucketListAdapter(childFragmentManager)
-
-        tabLayout.setupWithViewPager(viewPager)
 
         return view
 
